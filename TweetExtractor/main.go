52d4e876e3ff290dce888b9e0a3e6d494d7daa81
Mackyson <mackyson72@gmail.com>
2020-10-12 17:27:39 +0900

    コマンドライン引数で実行モードをしていすることに(UX最悪だけど、自分用なので……)

--- TweetExtractor/main.go
+++ TweetExtractor/main.go
@@ -4,12 +4,22 @@ import (
 	wrapper "TweetExtractor/APIwrapper"
 	textExtractor "TweetExtractor/TextExtractor"
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
+
+	mode := flag.String("mode", "", "search query")
+	op := flag.String("op", "", "option")
+	flag.Parse()
+
+	if *mode == "" {
+		fmt.Println("Specify the mode and op")
+		return
+	}
 
 	stdin := bufio.NewScanner(os.Stdin)
 
@@ -23,53 +33,58 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for _, tweet := range tweets {
-		tweetText := tweet.Status["text"].(string)
-		tweetId := tweet.Status["id_str"].(string)
-		// isRegistered := false //データが入っていない状態で登録済みチェックインを探すとエラーを吐くので，そのときはコメントアウトを外す
-		isRegistered, err := wrapper.CheckRegistered(es, tweetId)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if !isRegistered {
-			//チェックインツイートのリツイートを拾っていた場合
-			isRT := textExtractor.CheckWhetherRT(tweetText)
-			if isRT {
-				continue
+	if *mode == "register" {
+
+		for _, tweet := range tweets {
+			tweetText := tweet.Status["text"].(string)
+			tweetId := tweet.Status["id_str"].(string)
+			isRegistered := false
+			if *op != "new" {
+				isRegistered, err = wrapper.CheckRegistered(es, tweetId)
 			}
-			spotName := textExtractor.ExtractSpotName(tweetText)
-			log.Println(spotName)
-			//restaurantインデックスに登録済みのSpotだった場合
-			hasSameSpotDataInRestaurant, err := wrapper.CheckSameSpotDataInRestaurant(es, spotName)
 			if err != nil {
 				log.Fatal(err)
 			}
-			if hasSameSpotDataInRestaurant {
-				wrapper.RegisterAsRestaurant(es, tweet)
-				continue
-			}
-			//otherインデックスに登録済みのSpotだった場合
-			hasSameSpotDataInOther, err := wrapper.CheckSameSpotDataInOther(es, spotName)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if hasSameSpotDataInOther {
-				wrapper.RegisterAsOther(es, tweet)
-				continue
-			}
-			//その他の場合
-			fmt.Printf("%s", tweetText)
-			stdin.Scan()
-			isNewRestaurant := stdin.Text() == "y"
-			if isNewRestaurant {
-				err = wrapper.RegisterAsRestaurant(es, tweet)
+			if !isRegistered {
+				//チェックインツイートのリツイートを拾っていた場合
+				isRT := textExtractor.CheckWhetherRT(tweetText)
+				if isRT {
+					continue
+				}
+				spotName := textExtractor.ExtractSpotName(tweetText)
+				log.Println(spotName)
+				//restaurantインデックスに登録済みのSpotだった場合
+				hasSameSpotDataInRestaurant, err := wrapper.CheckSameSpotDataInRestaurant(es, spotName)
 				if err != nil {
 					log.Fatal(err)
 				}
-			} else {
-				err = wrapper.RegisterAsOther(es, tweet)
+				if hasSameSpotDataInRestaurant {
+					wrapper.RegisterAsRestaurant(es, tweet)
+					continue
+				}
+				//otherインデックスに登録済みのSpotだった場合
+				hasSameSpotDataInOther, err := wrapper.CheckSameSpotDataInOther(es, spotName)
 				if err != nil {
 					log.Fatal(err)
+				}
+				if hasSameSpotDataInOther {
+					wrapper.RegisterAsOther(es, tweet)
+					continue
+				}
+				//その他の場合
+				fmt.Printf("%s", tweetText)
+				stdin.Scan()
+				isNewRestaurant := stdin.Text() == "y"
+				if isNewRestaurant {
+					err = wrapper.RegisterAsRestaurant(es, tweet)
+					if err != nil {
+						log.Fatal(err)
+					}
+				} else {
+					err = wrapper.RegisterAsOther(es, tweet)
+					if err != nil {
+						log.Fatal(err)
+					}
 				}
 			}
 		}
